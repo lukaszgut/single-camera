56a3a918e2a4c1fdfec35a530f4446193cbd265e
Lukasz Gut <lgut@lgut.eu>
2025-06-24 11:39:23 +0200

    Add additional elements in streaming pipeline

--- src/single-camera-source/preprocessing.c
+++ src/single-camera-source/preprocessing.c
@@ -23,26 +23,33 @@ GstElement *create_preprocessing(void)
         return NULL;
     }
 
+    GstElement *vc_before = gst_element_factory_make("videoconvert", "vc_before");
     GstElement *effect = NULL;
+    GstElement *vc_after = gst_element_factory_make("videoconvert", "vc_after");
+
     if (calibration_mode) {
-	g_print("Creating cameracalibrate effect\n");
+        g_print("Creating cameracalibrate effect\n");
         effect = gst_element_factory_make("cameracalibrate", "cameracalibrate");
     } else {
         g_print("Creating cameraundistort effect\n");
         effect = gst_element_factory_make("cameraundistort", "cameraundistort");
     }
-    if (effect) {
-        gst_bin_add(GST_BIN(bin), effect);
+
+    if (vc_before && effect && vc_after) {
+        gst_bin_add_many(GST_BIN(bin), vc_before, effect, vc_after, NULL);
+        if (!gst_element_link_many(vc_before, effect, vc_after, NULL)) {
+            g_printerr("Failed to link videoconvert/effect chain in preprocessing\n");
+        }
     } else {
-        g_printerr("Failed to create effect element for preprocessing\n");
+        g_printerr("Failed to create videoconvert or effect element for preprocessing\n");
     }
 
-    GstPad *sink_pad = gst_element_get_static_pad(effect, "sink");
+    GstPad *sink_pad = gst_element_get_static_pad(vc_before, "sink");
     GstPad *ghost_sink = gst_ghost_pad_new("sink", sink_pad);
     gst_object_unref(sink_pad);
     gst_element_add_pad(bin, ghost_sink);
 
-    GstPad *src_pad = gst_element_get_static_pad(effect, "src");
+    GstPad *src_pad = gst_element_get_static_pad(vc_after, "src");
     GstPad *ghost_src = gst_ghost_pad_new("src", src_pad);
     gst_object_unref(src_pad);
     gst_element_add_pad(bin, ghost_src);
--- src/single-camera-source/streaming.c
+++ src/single-camera-source/streaming.c
@@ -7,19 +7,27 @@ GstElement *create_streaming(void)
 		g_printerr("Failed to create streaming bin\n");
 		return NULL;
 	}
+	GstElement *jpegenc = gst_element_factory_make("jpegenc", "jpegenc");
+	GstElement *avimux = gst_element_factory_make("avimux", "avimux");
 	GstElement *sink = gst_element_factory_make("tcpserversink", "tcp_sink");
-	if (!sink) {
+	if (!jpegenc || !avimux || !sink) {
+		g_printerr("Failed to create jpegenc, avimux, or sink\n");
+		if (jpegenc) gst_object_unref(jpegenc);
+		if (avimux) gst_object_unref(avimux);
+		if (sink) gst_object_unref(sink);
 		gst_object_unref(bin);
-		g_printerr("Failed to create sink\n");
 		return NULL;
 	}
 
 	// Set sink properties (example: host=0.0.0.0, port=5000)
 	g_object_set(sink, "host", "0.0.0.0", "port", 5000, NULL);
 
-	gst_bin_add(GST_BIN(bin), sink);
+	gst_bin_add_many(GST_BIN(bin), jpegenc, avimux, sink, NULL);
+	if (!gst_element_link_many(jpegenc, avimux, sink, NULL)) {
+		g_printerr("Failed to link jpegenc, avimux, and sink in streaming bin\n");
+	}
 
-	GstPad *pad = gst_element_get_static_pad(sink, "sink");
+	GstPad *pad = gst_element_get_static_pad(jpegenc, "sink");
 	GstPad *ghost_pad = gst_ghost_pad_new("sink", pad);
 	gst_object_unref(pad);
 	gst_element_add_pad(bin, ghost_pad);
